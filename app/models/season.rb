5ebed2069352e6c71a93a77a9973db184c07f55c
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-07-26 19:04:56 +0200

    Season model, controller and views created and modified for displaying what it is requested.

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,5 +1,5 @@
 class Season < ApplicationRecord
-  has_many :episodes, -> { order(:number) }, dependent: :destroy
+  has_many :episodes, dependent: :destroy
   has_many :purchase_options, as: :content
   has_many :libray_entries, as: :content
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,43 @@ require 'faker'
 
 puts 'Cleaning database...'
 Movie.destroy_all
+Season.destroy_all
+Episode.destroy_all
 
-puts 'Creating 100 fake movies...'
-100.times do
+puts 'Creating 50 fake movies...'
+50.times do
   movie = Movie.new(
     title: Faker::Movie.title,
     plot: Faker::Lorem.paragraph
   )
   movie.save!
+end
+puts 'Finished!'
+
+puts 'Creating 100 fake seasons...'
+50.times do
+  season = Season.new(
+    title: Faker::Book.title,
+    plot: Faker::Lorem.paragraph,
+    season_number: rand(1..10)
+  )
+  season.save!
+end
+puts 'Finished!'
+
+puts 'Creating 10 fake episodes for each season...'
+seasons = Season.all
+seasons.each do |season|
+  episode_number = 1
+  10.times do
+    episode = Episode.new(
+      title: Faker::Books::CultureSeries.book,
+      plot: Faker::Lorem.paragraph,
+      episode_number: episode_number,
+      season_id: season.id
+    )
+    episode.save!
+    episode_number += 1
+  end
 end
 puts 'Finished!'
