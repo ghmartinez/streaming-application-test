fcdd0287f5f71b20d322cba8a584f05cb26b8165
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-07-26 10:39:53 +0200

    Added filter for sorting movies in ascending and descending order in the index page.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,13 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+
+    # Check the sort parameter and adjust the order accordingly
+    if params[:sort] == "newest"
+      @movies = @movies.order(created_at: :desc)
+    elsif params[:sort] == "oldest"
+      @movies = @movies.order(created_at: :asc)
+    end
   end
 
   def show
