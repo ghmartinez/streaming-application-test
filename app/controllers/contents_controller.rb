35d35b9cf950fe5400d5d94a8eae8ce0f9d88438
Gabriel Martinez <ghmartinez96@hotmail.com>
2023-07-28 15:06:04 +0200

    Contents (movies and episodes) return a single list of movies and seasons, ordered by creation.

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,13 +2,19 @@ class ContentsController < ApplicationController
   def index
     @movies = Movie.all
     @seasons = Season.all
-    @contents = @movies + @seasons
+
+    # Create custom hashes for movies and seasons to include the type for sorting later
+    @movies_with_type = @movies.map { |movie| { content: movie, type: 'Movie' } }
+    @seasons_with_type = @seasons.map { |season| { content: season, type: 'Season' } }
+
+    # Combine both arrays and sort based on the 'created_at' attribute
+    @contents = (@movies_with_type + @seasons_with_type).sort_by { |c| c[:content].created_at }
 
     # Check the sort parameter and adjust the order accordingly
     if params[:sort] == "newest"
-      @contents = @contents.order(created_at: :desc)
+      @contents = @contents.reverse
     elsif params[:sort] == "oldest"
-      @contents = @contents.order(created_at: :asc)
+      @contents = @contents
     end
   end
 end
